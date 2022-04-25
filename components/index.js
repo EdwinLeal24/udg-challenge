16ae7eb54bc13e0d72f59aeba81286a2ecad2036
Edwin Leal <edwinleal019@gmail.com>
2022-04-25 22:09:38 +0200

    feat(components): index for components added

---
+++ components/index.js
@@ -0,0 +1,21 @@
+import { Header } from "@components/Header/Header";
+import { Footer } from "@components/Footer";
+import { Card } from "@components/Card";
+import { Picture } from "@components/Picture/index";
+import { Text } from "@components/Text";
+import { ColorPicker } from "@components/ColorPicker";
+import { CheckIcon } from "@components/Icons/CheckIcon";
+import { MemeCreatorBoxText } from "@components/MemeCreator/MemeCreatorBoxText";
+import { MemeCreatorEditMenu } from "@components/MemeCreator/MemeCreatorEditMenu";
+
+export {
+  Header,
+  Footer,
+  Card,
+  Picture,
+  Text,
+  CheckIcon,
+  ColorPicker,
+  MemeCreatorBoxText,
+  MemeCreatorEditMenu,
+};
