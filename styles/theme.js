9695db2f7458cbdfcad21a0c375193000ccd2c9e
Edwin Leal <edwinleal019@gmail.com>
2022-04-25 22:07:25 +0200

    styled: theme styles added

---
+++ styles/theme.js
@@ -0,0 +1,36 @@
+export const theme = {
+  colors: {
+    primary: "#8464c6",
+    secondary: "#54c59f",
+    purple: "#3d375e7f",
+    black: "#111111",
+    white: "#edecee",
+  },
+
+  spacing: {
+    space100: "16px",
+    space150: "20px",
+    space200: "24px",
+    space250: "32px",
+    space300: "40px",
+    space400: "56px",
+    space450: "72px",
+    space500: "120px",
+  },
+
+  sizes: {
+    size50: "8px",
+    size100: "10px",
+    size150: "12px",
+    size200: "16px",
+    size250: "24px",
+    size400: "32px",
+    size500: "48px",
+  },
+
+  fonts: {
+    comic: '"Comic Sans MS", arial',
+    arial: "Arial",
+    pacifico: "'Pacifico', cursive",
+  },
+};
